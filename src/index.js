5e8d07af8239d7ed86ccd1dea647fc49270e0c20
ArtVoron <artemvoronovnik@gmail.com>
2021-03-04 20:18:26 +0300

    commit#6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter } from 'react-router-dom';
